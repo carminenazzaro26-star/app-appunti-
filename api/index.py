3e3d2401586ddbc5f894989677886b9457f2b37c
Carmine Nazzaro <carminenazzaro26@gmail.com>
2026-05-11 23:38:25 +0200

    Debug WebRenderer attributes

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@
 import flet as ft
 import flet.fastapi as flet_fastapi
 from .database import db
+print(f"DEBUG: WebRenderer attributes: {dir(ft.WebRenderer)}")
 
 def main(page: ft.Page):
     page.title = "App Appunti"
