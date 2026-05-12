244a5c445b0e6f2e16504a520d8e975796ff70db
carminenazzaro26-star <carminenazzaro26-star@users.noreply.github.com>
2026-05-12 08:46:34 +0000

    deploy: 963736470cf217d47e54538e3d0da0dca2ac12db

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 // Le variabili reali vengono iniettate da GitHub Actions durante il deploy.
 // Per sviluppo locale, copia questo file in "config.js" e inserisci i tuoi valori.
 
-window.SUPABASE_URL = "https://fvariopykkztlmfaixxt.supabase.co";
-window.SUPABASE_KEY = "sb_publishable_S6Y_GPyRL2JAC0ZkVMK_Vg_SQ1ezOIY";
+window.SUPABASE_URL      = "https://fvariopykkztlmfaixxt.supabase.co";
+window.SUPABASE_KEY      = "sb_publishable_S6Y_GPyRL2JAC0ZkVMK_Vg_SQ1ezOIY";
+window.GOOGLE_CLIENT_ID  = "558916756213-93l4vvilembbp4iaua4lkgim1algb0m4.apps.googleusercontent.com";
